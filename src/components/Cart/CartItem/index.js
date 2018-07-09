395bd4f6301ffbc558e51f1c5405ba5d4b3f76e4
Raju Gandhi <raju.gandhi@gmail.com>
2018-07-09 19:36:41 -0400

    feat: make CartItem a functional component

--- src/components/Cart/CartItem/index.js
+++ src/components/Cart/CartItem/index.js
@@ -1,55 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CartItem extends React.Component {
-  render() {
-    const {
-      item,
-      quantity,
-      removeFromCart,
-      updateQuantity,
-    } = this.props;
+const CartItem = ({ item, quantity, removeFromCart, updateQuantity, }) => {
+  const format = (num) =>
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(num);
 
-    const format = (num) =>
-      new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      }).format(num);
-
-    const onChange = (e) => {
-      updateQuantity(item, e.currentTarget.value);
-    }
-
-    return <tr>
-      <td data-th="Product">
-        <div className="row">
-          <div className="col-sm-10">
-            <h4 className="nomargin">{item.name}</h4>
-            <p>{item.description}</p>
-          </div>
-        </div>
-      </td>
-      <td data-th="Price">${item.price}</td>
-      <td data-th="Quantity">
-        <input
-          type="number"
-          className="form-control text-center"
-          value={quantity}
-          onChange={onChange} />
-      </td>
-      <td data-th="Subtotal" className="text-center">
-        {format(quantity * item.price)}
-      </td>
-      <td className="actions" data-th="">
-        <button
-          className="btn btn-danger btn-sm"
-          onClick={() => removeFromCart(item)}
-        ><i className="fa fa-trash-o"></i></button>
-      </td>
-    </tr>
+  const onChange = (e) => {
+    updateQuantity(item, e.currentTarget.value);
   }
+
+  return <tr>
+    <td data-th="Product">
+      <div className="row">
+        <div className="col-sm-10">
+          <h4 className="nomargin">{item.name}</h4>
+          <p>{item.description}</p>
+        </div>
+      </div>
+    </td>
+    <td data-th="Price">${item.price}</td>
+    <td data-th="Quantity">
+      <input
+        type="number"
+        className="form-control text-center"
+        value={quantity}
+        onChange={onChange} />
+    </td>
+    <td data-th="Subtotal" className="text-center">
+      {format(quantity * item.price)}
+    </td>
+    <td className="actions" data-th="">
+      <button
+        className="btn btn-danger btn-sm"
+        onClick={() => removeFromCart(item)}
+      ><i className="fa fa-trash-o"></i></button>
+    </td>
+  </tr>
 }
 
 CartItem.propTypes = {
